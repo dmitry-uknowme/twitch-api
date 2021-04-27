e678f5b28a5bc6100bd7b4259e60bfceb9b3be20
dmitry-uknowme <45000651+dmitry-uknowme@users.noreply.github.com>
2021-04-27 23:05:24 +0500

    add next framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import Channel from './components/Channel/Channel';
 
 const App: React.FC = () => {
+	useEffect(() => {
+		//@ts-expect-error
+		const favoriteVideos = JSON.parse(localStorage.getItem('favoriteVideos'));
+		if (favoriteVideos) {
+		}
+	}, []);
+
 	return (
 		<div className='app'>
 			<Header />
